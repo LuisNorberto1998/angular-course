0ebd5b82ce178e50a3c01b4e814c41e7263ba827
Norberto P. Rodriguez <luis.paloma@ids.com.mx>
2022-12-31 17:38:41 -0600

    50.- Output - Componente incrementador

--- 03 Sección - EDNP/03 - adminPro/src/app/components/incrementador/incrementador.component.ts
+++ 03 Sección - EDNP/03 - adminPro/src/app/components/incrementador/incrementador.component.ts
@@ -1,27 +1,29 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-incrementador',
   templateUrl: './incrementador.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class IncrementadorComponent {
 
-  // @Input('valor') progreso:number = 20;
-  @Input() progreso:number = 20;
+  // @Input() progreso:number = 20;
+  @Input('valor') progreso: number = 20;
 
-  cambiarValor(valor:number){
-    
+  @Output('valor') valorSalida: EventEmitter<number> = new EventEmitter;
+
+  cambiarValor(valor: number) {
     if (this.progreso >= 100 && valor >= 0) {
-      return this.progreso = 100;
+      this.valorSalida.emit(100);
+      return (this.progreso = 100);
     }
-    
+
     if (this.progreso <= 0 && valor < 0) {
-      return this.progreso = 0;
+      this.valorSalida.emit(0);
+      return (this.progreso = 0);
     }
 
-    return this.progreso = this.progreso + valor;
+    this.valorSalida.emit(this.progreso);
+    return (this.progreso = this.progreso + valor);
   }
-
 }
