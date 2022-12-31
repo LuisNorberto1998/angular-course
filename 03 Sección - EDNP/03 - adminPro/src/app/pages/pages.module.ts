a8448118e1bf2fc457dd68f0cb95839cb9df5ece
Norberto P. Rodriguez <luis.paloma@ids.com.mx>
2022-12-31 11:29:35 -0600

    48.- Crear componente incrementador

--- 03 Sección - EDNP/03 - adminPro/src/app/pages/pages.module.ts
+++ 03 Sección - EDNP/03 - adminPro/src/app/pages/pages.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { ComponentsModule } from '../components/components.module';
 import { SharedModule } from '../shared/shared.module';
 import { RouterModule } from '@angular/router';
 
@@ -29,6 +30,7 @@ import { FormsModule } from '@angular/forms';
     CommonModule,
     SharedModule,
     RouterModule,
+    ComponentsModule
   ],
 })
 export class PagesModule {}
--- 03 Sección - EDNP/03 - adminPro/src/app/pages/progress/progress.component.ts
+++ 03 Sección - EDNP/03 - adminPro/src/app/pages/progress/progress.component.ts
@@ -7,23 +7,4 @@ import { Component } from '@angular/core';
 })
 export class ProgressComponent {
 
-  progreso:number = 20;
-
-  get getPorcentaje() {
-    return `${this.progreso}%`
-  }
-
-  cambiarValor(valor:number){
-    
-    if (this.progreso >= 100 && valor >= 0) {
-      return this.progreso = 100;
-    }
-    
-    if (this.progreso <= 0 && valor < 0) {
-      return this.progreso = 0;
-    }
-
-    return this.progreso = this.progreso + valor;
-  }
-
 }
