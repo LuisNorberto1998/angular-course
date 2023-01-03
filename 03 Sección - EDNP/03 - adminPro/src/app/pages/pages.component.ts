d1123b3b7245cf79159776eba2ac5e115365bfe7
Norberto P. Rodriguez <luis_norberto1998@hotmail.com>
2023-01-02 23:20:12 -0600

    63.- Cambiar el CSS principal de forma dinámica

--- 03 Sección - EDNP/03 - adminPro/src/app/pages/pages.component.ts
+++ 03 Sección - EDNP/03 - adminPro/src/app/pages/pages.component.ts
@@ -8,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PagesComponent implements OnInit {
 
+  public linkTheme = document.querySelector('#theme');
+
   constructor() { }
 
   ngOnInit(): void {
+    this.changeTheme();
+  }
+
+  changeTheme() {
+    
+    const themeStorage = localStorage.getItem('theme') || './assets/css/colors/default.css';
+    this.linkTheme?.setAttribute('href', themeStorage);
+
   }
 
 }
