8e92312f3e61aa9673a7818283c717b2cf6f21e2
Norberto P. Rodriguez <luis_norberto1998@hotmail.com>
2023-01-02 21:32:35 -0600

    53-54.- Gráficas en Angular

--- 03 Sección - EDNP/03 - adminPro/src/app/pages/grafica1/grafica1.component.ts
+++ 03 Sección - EDNP/03 - adminPro/src/app/pages/grafica1/grafica1.component.ts
@@ -1,16 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { MultiDataSet, Label, Color } from 'ng2-charts';
 
 @Component({
   selector: 'app-grafica1',
   templateUrl: './grafica1.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class Grafica1Component implements OnInit {
+export class Grafica1Component {
+  // Doughnut
+  public doughnutChartLabels: Label[] = [
+    'Download Sales',
+    'In-Store Sales',
+    'Mail-Order Sales',
+  ];
 
-  constructor() { }
+  public doughnutChartData: MultiDataSet = [
+    [350, 450, 100],
+  ];
 
-  ngOnInit(): void {
-  }
+  public colors:Color[] = [
+    {backgroundColor: [ '#6857E6', '#009FEE', '#F02059']}
+  ]
 
 }
--- 03 Sección - EDNP/03 - adminPro/src/app/pages/pages.module.ts
+++ 03 Sección - EDNP/03 - adminPro/src/app/pages/pages.module.ts
@@ -5,6 +5,8 @@ import { ComponentsModule } from '../components/components.module';
 import { SharedModule } from '../shared/shared.module';
 import { RouterModule } from '@angular/router';
 
+import { ChartsModule } from 'ng2-charts';
+
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
@@ -26,6 +28,7 @@ import { FormsModule } from '@angular/forms';
     PagesComponent,
   ],
   imports: [
+    ChartsModule,
     FormsModule,
     CommonModule,
     SharedModule,
